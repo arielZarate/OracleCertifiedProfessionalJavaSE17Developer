d0e27cbd1e59ce82d3cfba0465fe07c397f3bffe
arielZarate <arieltecnico@gmail.com>
2025-10-21 20:49:39 -0300

    remove project spring and unified with repo springBoot

--- spring/autowiredExample/src/main/java/arielzarate/arielzarate/UsuarioService.java
+++
@@ -1,8 +0,0 @@
-package arielzarate.arielzarate;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UsuarioService {
-}
--- spring/springdata/src/main/java/com/arielzarate/infraestructure/persistence/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.arielzarate.infraestructure.persistence.repositories;
-
-import com.arielzarate.infraestructure.persistence.model.ClientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository  extends JpaRepository<ClientEntity,Long> {
-}
